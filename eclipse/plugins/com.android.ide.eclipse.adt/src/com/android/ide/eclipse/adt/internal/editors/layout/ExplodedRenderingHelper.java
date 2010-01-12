e13151727c63786342cddc3ea355425582bd4e7a
The Android Open Source Project <initial-contribution@android.com>
2010-01-12 15:19:28 -0800

    android-2.1_r1 snapshot

---
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/layout/ExplodedRenderingHelper.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ide.eclipse.adt.internal.editors.layout;
+
+import com.android.ide.eclipse.adt.internal.editors.descriptors.ElementDescriptor;
+import com.android.ide.eclipse.adt.internal.editors.layout.descriptors.LayoutDescriptors;
+import com.android.ide.eclipse.adt.internal.editors.uimodel.UiElementNode;
+import com.android.ide.eclipse.adt.internal.sdk.AndroidTargetData;
+import com.android.ide.eclipse.adt.internal.sdk.Sdk;
+import com.android.sdklib.IAndroidTarget;
+
+import org.eclipse.core.resources.IProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class computes the new screen size in "exploded rendering" mode.
+ * It goes through the whole layout tree and figures out how many embedded layouts will have
+ * extra padding and compute how that will affect the screen size.
+ *
+ * TODO
+ * - find a better class name :)
+ * - move the logic for each layout to groovy scripts?
+ * - support custom classes (by querying JDT for its super class and reverting to its behavior)
+ */
+public final class ExplodedRenderingHelper {
+    /** value of the padding in pixel.
+     * TODO: make a preference?
+     */
+    public final static int PADDING_VALUE = 10;
+
+    private final int[] mPadding = new int[] { 0, 0 };
+    private List<ElementDescriptor> mLayoutDescriptors;
+
+    public ExplodedRenderingHelper(UiElementNode top, IProject iProject) {
+        // get the layout descriptor
+        IAndroidTarget target = Sdk.getCurrent().getTarget(iProject);
+        AndroidTargetData data = Sdk.getCurrent().getTargetData(target);
+        LayoutDescriptors descriptors = data.getLayoutDescriptors();
+        mLayoutDescriptors = descriptors.getLayoutDescriptors();
+
+        computePadding(top, mPadding);
+    }
+
+    /**
+     * Returns the number of extra padding in the X axis. This doesn't return a number of pixel
+     * or dip, but how many paddings are pushing the screen dimension out.
+     */
+    public int getWidthPadding() {
+        return mPadding[0];
+    }
+
+    /**
+     * Returns the number of extra padding in the Y axis. This doesn't return a number of pixel
+     * or dip, but how many paddings are pushing the screen dimension out.
+     */
+    public int getHeightPadding() {
+        return mPadding[1];
+    }
+
+    /**
+     * Computes the number of padding for a given view, and fills the given array of int.
+     * <p/>index 0 is X axis, index 1 is Y axis
+     * @param view the view to compute
+     * @param padding the result padding (index 0 is X axis, index 1 is Y axis)
+     */
+    private void computePadding(UiElementNode view, int[] padding) {
+        String localName = view.getDescriptor().getXmlLocalName();
+
+        // first compute for each children
+        List<UiElementNode> children = view.getUiChildren();
+        int count = children.size();
+        if (count > 0) {
+            // compute the padding for all the children.
+            List<int[]> childrenPadding = new ArrayList<int[]>(count);
+            for (int i = 0 ; i < count ; i++) {
+                UiElementNode child = children.get(i);
+                int[] p = new int[] { 0, 0 };
+                childrenPadding.add(p);
+                computePadding(child, p);
+            }
+
+            // now combine/compare based on the parent.
+            // TODO: need a better way to do this, groovy or other.
+            if (count == 1) {
+                int[] p = childrenPadding.get(0);
+                padding[0] = p[0];
+                padding[1] = p[1];
+            } else {
+                if ("LinearLayout".equals(localName)) {
+                    // TODO: figure out the orientation of the layout
+                    combineLinearLayout(childrenPadding, padding, false);
+                } else if ("TableLayout".equals(localName)) {
+                    combineLinearLayout(childrenPadding, padding, false);
+                } else if ("TableRow".equals(localName)) {
+                    combineLinearLayout(childrenPadding, padding, true);
+                } else {
+                    // TODO: need to figure out what we do here
+                    throw new UnsupportedOperationException();
+                }
+            }
+        }
+
+        // if the view itself is a layout, add its padding
+        for (ElementDescriptor desc : mLayoutDescriptors) {
+            if (localName.equals(desc.getXmlName())) {
+                padding[0]++;
+                padding[1]++;
+                break;
+            }
+        }
+    }
+
+    private void combineLinearLayout(List<int[]> paddings, int[] resultPadding,
+            boolean horizontal) {
+        // The way the children are combined will depend on the direction.
+        // For instance in a vertical layout, we add the y padding as they all add to the length
+        // of the needed canvas, while we take the biggest x padding needed by the children
+
+        // the axis in which we take the sum of the padding of the children
+        int sumIndex = horizontal ? 0 : 1;
+        // the axis in which we take the max of the padding of the children
+        int maxIndex = horizontal ? 1 : 0;
+
+        int max = -1;
+        for (int[] p : paddings) {
+            resultPadding[sumIndex] += p[sumIndex];
+            if (max == -1 || max < p[maxIndex]) {
+                max = p[maxIndex];
+            }
+        }
+        resultPadding[maxIndex] = max;
+    }
+}
---
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/preferences/AdtPrefs.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Eclipse Public License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.eclipse.org/org/documents/epl-v10.php
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.ide.eclipse.adt.internal.preferences;
+
+import com.android.ide.eclipse.adt.AdtPlugin;
+import com.android.jarutils.DebugKeyProvider;
+import com.android.jarutils.DebugKeyProvider.KeytoolException;
+import com.android.prefs.AndroidLocation.AndroidLocationException;
+
+import org.eclipse.core.runtime.Preferences.PropertyChangeEvent;
+import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
+
+import java.io.File;
+
+@SuppressWarnings("deprecation")
+public final class AdtPrefs extends AbstractPreferenceInitializer {
+    public final static String PREFS_SDK_DIR = AdtPlugin.PLUGIN_ID + ".sdk"; //$NON-NLS-1$
+
+    public final static String PREFS_BUILD_RES_AUTO_REFRESH = AdtPlugin.PLUGIN_ID + ".resAutoRefresh"; //$NON-NLS-1$
+
+    public final static String PREFS_BUILD_FORCE_ERROR_ON_NATIVELIB_IN_JAR = AdtPlugin.PLUGIN_ID + ".forceErrorNativeLibInJar"; //$NON-NLS-1$
+
+    public final static String PREFS_BUILD_VERBOSITY = AdtPlugin.PLUGIN_ID + ".buildVerbosity"; //$NON-NLS-1$
+
+    public final static String PREFS_DEFAULT_DEBUG_KEYSTORE = AdtPlugin.PLUGIN_ID + ".defaultDebugKeyStore"; //$NON-NLS-1$
+
+    public final static String PREFS_CUSTOM_DEBUG_KEYSTORE = AdtPlugin.PLUGIN_ID + ".customDebugKeyStore"; //$NON-NLS-1$
+
+    public final static String PREFS_HOME_PACKAGE = AdtPlugin.PLUGIN_ID + ".homePackage"; //$NON-NLS-1$
+
+    public final static String PREFS_EMU_OPTIONS = AdtPlugin.PLUGIN_ID + ".emuOptions"; //$NON-NLS-1$
+
+    /** singleton instance */
+    private final static AdtPrefs sThis = new AdtPrefs();
+
+    /** default store, provided by eclipse */
+    private IPreferenceStore mStore;
+
+    /** cached location for the sdk folder */
+    private String mOsSdkLocation;
+
+    /** Verbosity of the build */
+    private BuildVerbosity mBuildVerbosity = BuildVerbosity.NORMAL;
+
+    private boolean mBuildForceResResfresh = false;
+    private boolean mBuildForceErrorOnNativeLibInJar = true;
+
+    public static enum BuildVerbosity {
+        /** Build verbosity "Always". Those messages are always displayed, even in silent mode */
+        ALWAYS(0),
+        /** Build verbosity level "Normal" */
+        NORMAL(1),
+        /** Build verbosity level "Verbose". Those messages are only displayed in verbose mode */
+        VERBOSE(2);
+
+        private int mLevel;
+
+        BuildVerbosity(int level) {
+            mLevel = level;
+        }
+
+        public int getLevel() {
+            return mLevel;
+        }
+
+        /**
+         * Finds and returns a {@link BuildVerbosity} whose {@link #name()} matches a given name.
+         * <p/>This is different from {@link Enum#valueOf(Class, String)} in that it returns null
+         * if no matches are found.
+         *
+         * @param name the name to look up.
+         * @return returns the matching enum or null of no match where found.
+         */
+        public static BuildVerbosity find(String name) {
+            for (BuildVerbosity v : values()) {
+                if (v.name().equals(name)) {
+                    return v;
+                }
+            }
+
+            return null;
+        }
+    }
+
+    public static void init(IPreferenceStore preferenceStore) {
+        sThis.mStore = preferenceStore;
+    }
+
+    public static AdtPrefs getPrefs() {
+        return sThis;
+    }
+
+    public synchronized void loadValues(PropertyChangeEvent event) {
+        // get the name of the property that changed, if any
+        String property = event != null ? event.getProperty() : null;
+
+        if (property == null || PREFS_SDK_DIR.equals(property)) {
+            mOsSdkLocation = mStore.getString(PREFS_SDK_DIR);
+
+            // make sure it ends with a separator. Normally this is done when the preference
+            // is set. But to make sure older version still work, we fix it here as well.
+            if (mOsSdkLocation.length() > 0 && mOsSdkLocation.endsWith(File.separator) == false) {
+                mOsSdkLocation = mOsSdkLocation + File.separator;
+            }
+        }
+
+        if (property == null || PREFS_BUILD_VERBOSITY.equals(property)) {
+            mBuildVerbosity = BuildVerbosity.find(mStore.getString(PREFS_BUILD_VERBOSITY));
+            if (mBuildVerbosity == null) {
+                mBuildVerbosity = BuildVerbosity.NORMAL;
+            }
+        }
+
+        if (property == null || PREFS_BUILD_RES_AUTO_REFRESH.equals(property)) {
+            mBuildForceResResfresh = mStore.getBoolean(PREFS_BUILD_RES_AUTO_REFRESH);
+        }
+
+        if (property == null || PREFS_BUILD_FORCE_ERROR_ON_NATIVELIB_IN_JAR.equals(property)) {
+            mBuildForceErrorOnNativeLibInJar = mStore.getBoolean(PREFS_BUILD_RES_AUTO_REFRESH);
+        }
+    }
+
+    /**
+     * Returns the SDK folder.
+     * Guaranteed to be terminated by a platform-specific path separator.
+     */
+    public synchronized String getOsSdkFolder() {
+        return mOsSdkLocation;
+    }
+
+    public synchronized BuildVerbosity getBuildVerbosity() {
+        return mBuildVerbosity;
+    }
+
+    public  boolean getBuildForceResResfresh() {
+        return mBuildForceResResfresh;
+    }
+
+    public boolean getBuildForceErrorOnNativeLibInJar() {
+        return mBuildForceErrorOnNativeLibInJar;
+    }
+
+    @Override
+    public void initializeDefaultPreferences() {
+        IPreferenceStore store = AdtPlugin.getDefault().getPreferenceStore();
+
+        store.setDefault(PREFS_BUILD_RES_AUTO_REFRESH, true);
+        store.setDefault(PREFS_BUILD_FORCE_ERROR_ON_NATIVELIB_IN_JAR, true);
+
+        store.setDefault(PREFS_BUILD_VERBOSITY, BuildVerbosity.ALWAYS.name());
+
+        store.setDefault(PREFS_HOME_PACKAGE, "android.process.acore"); //$NON-NLS-1$
+
+        try {
+            store.setDefault(PREFS_DEFAULT_DEBUG_KEYSTORE,
+                    DebugKeyProvider.getDefaultKeyStoreOsPath());
+        } catch (KeytoolException e) {
+            AdtPlugin.log(e, "Get default debug keystore path failed"); //$NON-NLS-1$
+        } catch (AndroidLocationException e) {
+            AdtPlugin.log(e, "Get default debug keystore path failed"); //$NON-NLS-1$
+        }
+    }
+}
