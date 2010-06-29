b6fd70d47d0d9ac4080ffb3d58bcf55375453561
Raphael Moll <ralf@android.com>
2010-06-29 14:52:16 -0700

    ADT: Fix NPE in ConfigComposite
    For some reason the locale used by the ConfigState in the
    ConfigurationComposite had null members. Failed to reproduce
    it. One clue might be there was a non-selected GLE2 open
    as an editor tab but the Java workspace was not the main
    visible one. It's possible some event fired before the
    composite controls got created.
    
    Change-Id: I2c8b6c86842f3c772c2a7ec113941eb8fc01b9e4

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/layout/configuration/ConfigurationComposite.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/editors/layout/configuration/ConfigurationComposite.java
@@ -189,9 +189,12 @@ public class ConfigurationComposite extends Composite {
                 sb.append(configName);
                 sb.append(SEP);
                 if (locale != null) {
-                    sb.append(((LanguageQualifier) locale[0]).getValue());
-                    sb.append(SEP_LOCALE);
-                    sb.append(((RegionQualifier) locale[1]).getValue());
+                    if (locale[0] != null && locale[1] != null) {
+                        // locale[0]/[1] can be null sometimes when starting Eclipse
+                        sb.append(((LanguageQualifier) locale[0]).getValue());
+                        sb.append(SEP_LOCALE);
+                        sb.append(((RegionQualifier) locale[1]).getValue());
+                    }
                 }
                 sb.append(SEP);
                 sb.append(theme);
