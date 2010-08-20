e3f00462485543b752bbc3b15db0b2b6920e34e1
Raphael Moll <ralf@android.com>
2010-08-19 20:56:26 -0700

    UnitTest for UpdaterLogic.findPlatformToolsDependency.
    Change-Id: Ie49a141ad0d25021164166aeb4405877045719bf

--- sdkmanager/libs/sdkuilib/tests/com/android/sdkuilib/internal/repository/UpdaterLogicTest.java
+++ sdkmanager/libs/sdkuilib/tests/com/android/sdkuilib/internal/repository/UpdaterLogicTest.java
@@ -19,6 +19,7 @@ package com.android.sdkuilib.internal.repository;
 import com.android.sdklib.internal.repository.Archive;
 import com.android.sdklib.internal.repository.MockAddonPackage;
 import com.android.sdklib.internal.repository.MockPlatformPackage;
+import com.android.sdklib.internal.repository.MockPlatformToolPackage;
 import com.android.sdklib.internal.repository.MockToolPackage;
 import com.android.sdklib.internal.repository.Package;
 import com.android.sdklib.internal.repository.RepoSource;
@@ -48,7 +49,12 @@ public class UpdaterLogicTest extends TestCase {
         }
     }
 
-    public void testFindAddonDependency() throws Exception {
+    /**
+     * Addon packages depend on a base platform package.
+     * This test checks that UpdaterLogic.findPlatformToolsDependency(...)
+     * can find the base platform for a given addon.
+     */
+    public void testFindAddonDependency() {
         MockUpdaterLogic mul = new MockUpdaterLogic(null);
 
         MockPlatformPackage p1 = new MockPlatformPackage(1, 1);
@@ -68,7 +74,7 @@ public class UpdaterLogicTest extends TestCase {
         RepoSource[] sources = null;
 
         // a2 now depends on a "fake" archive info with no newArchive that wraps the missing
-        // underlying platform
+        // underlying platform.
         ArchiveInfo fai = mul.findPlatformDependency(a2, out, selected, remote, sources, locals);
         assertNotNull(fai);
         assertNull(fai.getNewArchive());
@@ -85,11 +91,18 @@ public class UpdaterLogicTest extends TestCase {
         assertSame(p2_archive, out.get(0).getNewArchive());
     }
 
-    public void testFindPlatformDependency() throws Exception {
+    /**
+     * Platform packages depend on a tool package.
+     * This tests checks that UpdaterLogic.findToolsDependency() can find a base
+     * tool package for a given platform package.
+     */
+    public void testFindPlatformDependency() {
         MockUpdaterLogic mul = new MockUpdaterLogic(null);
 
-        MockToolPackage t1 = new MockToolPackage(1);
-        MockToolPackage t2 = new MockToolPackage(2);
+        MockPlatformToolPackage pt1 = new MockPlatformToolPackage(1);
+
+        MockToolPackage t1 = new MockToolPackage(1, 1);
+        MockToolPackage t2 = new MockToolPackage(2, 1);
 
         MockPlatformPackage p2 = new MockPlatformPackage(2, 1, 2);
 
@@ -98,7 +111,7 @@ public class UpdaterLogicTest extends TestCase {
         ArrayList<Package> remote = new ArrayList<Package>();
 
         // p2 depends on t2, which is not locally installed
-        Package[] localPkgs = { t1 };
+        Package[] localPkgs = { t1, pt1 };
         ArchiveInfo[] locals = mul.createLocalArchives(localPkgs);
 
         RepoSource[] sources = null;
@@ -115,6 +128,49 @@ public class UpdaterLogicTest extends TestCase {
         Archive t2_archive = t2.getArchives()[0];
         selected.add(t2_archive);
         ArchiveInfo ai2 = mul.findToolsDependency(p2, out, selected, remote, sources, locals);
+        assertNotNull(ai2);
+        assertSame(t2_archive, ai2.getNewArchive());
+        assertEquals(1, out.size());
+        assertSame(t2_archive, out.get(0).getNewArchive());
+    }
+
+    /**
+     * Tool packages require a platform-tool package to be present or installed.
+     * This tests checks that UpdaterLogic.findPlatformToolsDependency() can find a base
+     * platform-tool package for a given tool package.
+     */
+    public void testFindPlatformToolDependency() {
+        MockUpdaterLogic mul = new MockUpdaterLogic(null);
+
+        MockPlatformToolPackage t1 = new MockPlatformToolPackage(1);
+        MockPlatformToolPackage t2 = new MockPlatformToolPackage(2);
+
+        MockToolPackage p2 = new MockToolPackage(2, 2);
+
+        ArrayList<ArchiveInfo> out = new ArrayList<ArchiveInfo>();
+        ArrayList<Archive> selected = new ArrayList<Archive>();
+        ArrayList<Package> remote = new ArrayList<Package>();
+
+        // p2 depends on t2, which is not locally installed
+        Package[] localPkgs = { t1 };
+        ArchiveInfo[] locals = mul.createLocalArchives(localPkgs);
+
+        RepoSource[] sources = null;
+
+        // p2 now depends on a "fake" archive info with no newArchive that wraps the missing
+        // underlying tool
+        ArchiveInfo fai = mul.findPlatformToolsDependency(
+                                    p2, out, selected, remote, sources, locals);
+        assertNotNull(fai);
+        assertNull(fai.getNewArchive());
+        assertTrue(fai.isRejected());
+        assertEquals(0, out.size());
+
+        // t2 is now selected and can be used as a dependency
+        Archive t2_archive = t2.getArchives()[0];
+        selected.add(t2_archive);
+        ArchiveInfo ai2 = mul.findPlatformToolsDependency(
+                                    p2, out, selected, remote, sources, locals);
         assertNotNull(ai2);
         assertSame(t2_archive, ai2.getNewArchive());
         assertEquals(1, out.size());
