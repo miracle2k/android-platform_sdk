b0c49c6f495609f15a5df2b2d062ac0b4776f72b
Konstantin Lopyrev <klopyrev@google.com>
2010-08-25 12:28:47 -0700

    Renaming
    Change-Id: I10c8b1cf1be3b66fe0b7527ffb72501d8d61ebb3

--- hierarchyviewer/etc/Android.mk
+++ hierarchyviewer/etc/Android.mk
@@ -15,6 +15,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_PREBUILT_EXECUTABLES := hierarchyviewer
+LOCAL_PREBUILT_EXECUTABLES := hierarchyviewer1
 include $(BUILD_HOST_PREBUILT)
 
