85a5207aea2b5a1b49ef1731e17546e794516010
Xavier Ducrohet <xav@android.com>
2010-12-06 16:56:44 -0800

    New test project for Eclipse: referencing java projects.
    Change-Id: I4ac4f89ad4c40c0073d7c24662f5a8e8740b696a

---
+++ testapps/basicProjectWithJava/src/com/basicProjectWithJava/Main.java
@@ -0,0 +1,14 @@
+package com.basicProjectWithJava;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+}
