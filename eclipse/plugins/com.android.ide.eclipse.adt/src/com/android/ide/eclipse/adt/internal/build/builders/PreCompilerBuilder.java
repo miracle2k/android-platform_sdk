aaa1ca81235e8b92dd59119c4cf145ad83a185fe
Raphael Moll <ralf@android.com>
2011-01-17 16:21:43 -0800

    Display AIDL cmd line in ADT Build Verbose mode.
    Change-Id: I9d9091baac364671f0698becd33c87fba41edc7d

--- eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/build/builders/PreCompilerBuilder.java
+++ eclipse/plugins/com.android.ide.eclipse.adt/src/com/android/ide/eclipse/adt/internal/build/builders/PreCompilerBuilder.java
@@ -1022,6 +1022,16 @@ public class PreCompilerBuilder extends BaseBuilder {
     private boolean execAidl(String[] command, IFile file) {
         // do the exec
         try {
+            if (AdtPrefs.getPrefs().getBuildVerbosity() == BuildVerbosity.VERBOSE) {
+                StringBuilder sb = new StringBuilder();
+                for (String c : command) {
+                    sb.append(c);
+                    sb.append(' ');
+                }
+                String cmd_line = sb.toString();
+                AdtPlugin.printToConsole(getProject(), cmd_line);
+            }
+
             Process p = Runtime.getRuntime().exec(command);
 
             // list to store each line of stderr
